62b9c02d1458690c13a5105a2e8970a6140c81be
mixa marciv <mixamarciv@gmail.com>
2014-09-29 10:04:58 +0400

    3

--- index.js
+++ index.js
@@ -13,31 +13,43 @@ module.exports = process_logger;
 
 function process_logger(p,fn) {
   if(!p.run) return fn(new Error("ОШИБКА: не задан параметр run"));
-  if(!p.log) return fn(new Error("ОШИБКА: не задан параметр log"));
+  if(!p.log){
+    p.__write_log_to_file = 0;
+    //return fn(new Error("ОШИБКА: не задан параметр log"));
+  }else{
+    p.__write_log_to_file = 1;
+  }
   if (!p.args && p.arguments) p.args = p.arguments;
   else if (p.args && p.arguments) p.args.concat(p.arguments);
   
-  var fs = require('fs');
-  var log = fs.createWriteStream(p.log, {flags: 'w'});
-  //далее переопределяем функцию записи, для одновременного вывода в консоль и записи в лог с датой и временем
-  log._write_old = log._write;
-  var i_call = 0;
-  var show_time_in_log = 1;
-  log._write = function(chunk, enc, next){ 
-      if(++i_call == 1 || !show_time_in_log) return log._write_old.apply(this,arguments);  //первый кусок почему то вызывается по 2 раза
-      var i_call0 = i_call - 1;
-      var str = chunk.toString();
-      if(isFunction(p.on_data)){
-          //process.stdout.write(str);  //выводим в консоль то что получили..
-          p.on_data(str,i_call0);
+  var log = {};
+  if (p.__write_log_to_file) {
+      var fs = require('fs');
+      log = fs.createWriteStream(p.log, {flags: 'w'});
+      //далее переопределяем функцию записи, для одновременного вывода в консоль и записи в лог с датой и временем
+      log._write_old = log._write;
+      var i_call = 0;
+      var show_time_in_log = 1;
+      log._write = function(chunk, enc, next){ 
+          if(++i_call == 1 || !show_time_in_log) return log._write_old.apply(this,arguments);  //первый кусок почему то вызывается по 2 раза
+          var i_call0 = i_call - 1;
+          var str = chunk.toString();
+          if(isFunction(p.on_data)){
+              //process.stdout.write(str);  //выводим в консоль то что получили..
+              p.on_data(str,i_call0);
+          }
+          str = prepare_str_to_log(str,i_call0);
+          
+          arguments[0] = new Buffer(str, p.enc); //заменяем строку для записи на новую
+          log._write_old.apply(this,arguments);
       }
-      str = prepare_str_to_log(str,i_call0);
       
-      arguments[0] = new Buffer(str, p.enc); //заменяем строку для записи на новую
-      log._write_old.apply(this,arguments);
+      log.write('\n');
+  }else{
+      log.write = process.stdout.write;
+      log.end = function(){};
   }
-  
-  log.write('\n');
+
   if (p.debug) {
     var util = require('util');  
     log.write('\nstart app:'+p.run+' log:'+p.log+' args:'+util.inspect(p.args)+'\n');
